a4fbce57d9e0a460294abc5f67ac2deb2537602e
Selim Aktay <sellimakty99@gmail.com>
2022-08-22 22:11:21 +0300

    Ödev

--- app.js
+++ app.js
@@ -1,13 +1,26 @@
 const express = require("express");
+const ejs = require("ejs");
+
 const app = express();
 
+//template engine
+app.set("view engine", "ejs");
+
+//middlewares
+app.use(express.static("public"));
+
+//routes
 app.get("/", (req, res) => {
-  const blog = {
-    id: 1,
-    title: "Blog Title",
-    description: "Blog description",
-  };
-  res.send(blog);
+  //res.sendFile(path.resolve(__dirname, 'temp/index.html'));
+  res.render("index");
+});
+app.get("/about", (req, res) => {
+  //res.sendFile(path.resolve(__dirname, 'temp/index.html'));
+  res.render("about");
+});
+app.get("/add", (req, res) => {
+  //res.sendFile(path.resolve(__dirname, 'temp/index.html'));
+  res.render("add");
 });
 
 const port = 3000;
